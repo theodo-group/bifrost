aec30881d30f70c85708470e5e9b8dde1d013422
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2022-10-10 11:05:12 +0200

    🔥 Remove unused nx-plugin

--- packages/nx-plugin/src/generators/helpers/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './createFiles';
-export * from './normalizeOptions';
-export * from './packageGenerator';
-export * from './symlink';
-export * from './updateCodeWorkspace';
-export * from './updatePackages';
-export * from './updateRootPackageJson';
--- packages/nx-plugin/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as libraryGenerator } from './generators/library';
-export { default as serviceGenerator } from './generators/service';
