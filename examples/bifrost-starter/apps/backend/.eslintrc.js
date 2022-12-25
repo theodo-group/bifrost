02c93aa6007f23aed2223c00daf39ce65d9bf9f2
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2022-12-25 13:51:43 +0100

    chore: share eslint as a package

--- examples/bifrost-starter/apps/backend/.eslintrc.js
+++ examples/bifrost-starter/apps/backend/.eslintrc.js
@@ -1,48 +1,7 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  root: true,
+  extends: ['custom'],
   rules: {
-    curly: ['error', 'all'],
-    eqeqeq: ['error', 'smart'],
-    complexity: ['error', 8],
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-        optionalDependencies: false,
-        peerDependencies: false,
-      },
-    ],
-    'no-shadow': [
-      'error',
-      {
-        hoist: 'all',
-      },
-    ],
-    'prefer-const': 'error',
-    'unused-imports/no-unused-imports-ts': 'error',
-    'import/order': [
-      'error',
-      {
-        groups: [['external', 'builtin'], 'internal', ['parent', 'sibling', 'index']],
-      },
-    ],
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-      },
-    ],
-    'padding-line-between-statements': [
-      'error',
-      {
-        blankLine: 'always',
-        prev: '*',
-        next: 'return',
-      },
-    ],
     'no-restricted-imports': [
       'error',
       {
@@ -56,47 +15,5 @@ module.exports = {
         ],
       },
     ],
-  },
-  root: true,
-  plugins: ['import', 'unused-imports'],
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-    jest: true,
-  },
-  overrides: [
-    {
-      files: ['**/*.ts?(x)'],
-      extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: ['./tsconfig.json'],
-        tsconfigRootDir: __dirname,
-      },
-      rules: {
-        '@typescript-eslint/prefer-optional-chain': 'error',
-        'no-shadow': 'off',
-        '@typescript-eslint/no-shadow': 'error',
-        '@typescript-eslint/restrict-template-expressions': [
-          'error',
-          {
-            allowNumber: true,
-            allowBoolean: true,
-          },
-        ],
-        '@typescript-eslint/strict-boolean-expressions': [
-          'error',
-          {
-            allowString: false,
-            allowNumber: false,
-            allowNullableObject: true,
-          },
-        ],
-      },
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: 2022,
   },
 };
