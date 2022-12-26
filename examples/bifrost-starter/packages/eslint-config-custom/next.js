ff0ec754b89f886357c4e4e222d5ce67d6ea3b0a
jeremiec <Jeremie-Chauvel@users.noreply.github.com>
2022-12-26 13:09:40 +0100

    chore: add shared next eslint config

---
+++ examples/bifrost-starter/packages/eslint-config-custom/next.js
@@ -0,0 +1,63 @@
+module.exports = {
+  plugins: ['jsx-a11y', 'risxss'],
+  extends: ['next/core-web-vitals', './base', 'plugin:jsx-a11y/strict'],
+  rules: {
+    // Specific NextJS accessibility config
+    // Not necessary with NextJS links
+    'jsx-a11y/anchor-is-valid': 'off',
+    // Add alt attributes to NextJS Images
+    'jsx-a11y/alt-text': [
+      2,
+      {
+        img: ['Image'],
+      },
+    ],
+    'risxss/catch-potential-xss-react': 'error',
+    'react/no-string-refs': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+      extends: ['./base'],
+      rules: {
+        'react-hooks/exhaustive-deps': 'error',
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            types: {
+              FC: 'Use `const MyComponent = (props: Props): JSX.Element` instead',
+              SFC: 'Use `const MyComponent = (props: Props): JSX.Element` instead',
+              FunctionComponent:
+                'Use `const MyComponent = (props: Props): JSX.Element` instead',
+              'React.FC':
+                'Use `const MyComponent = (props: Props): JSX.Element` instead',
+              'React.SFC':
+                'Use `const MyComponent = (props: Props): JSX.Element` instead',
+              'React.FunctionComponent':
+                'Use `const MyComponent = (props: Props): JSX.Element` instead',
+            },
+            extendDefaults: true,
+          },
+        ],
+      },
+    },
+  ],
+};
